d84f6ccdd9429b1c48099cd10da244eb1eef3869
Himanshu Mishra <166356154+Himanshu6408@users.noreply.github.com>
2025-07-22 21:22:35 +0530

    reume changed

---
+++ src/components/About/About.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+import Tilt from "react-parallax-tilt";
+import profileImage from "../../assets/profile.jpg";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-39"
+    >
+      <div className="flex flex-col items-center text-center">
+        {/* Profile Image on Top */}
+        <div className="mb-10">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[20.4rem] md:h-[21rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Himanshu Mishra"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+
+        {/* Text Content */}
+        <div className="w-full max-w-3xl">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Himanshu Mishra
+          </h2>
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={["Frontend Developer"]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a frontend developer with a strong passion for creating
+            user-friendly and responsive web applications. Currently focused on
+            mastering frontend technologies like HTML, CSS, JavaScript, and
+            React. I aim to build clean, interactive interfaces that enhance
+            user experience. With continuous learning, I plan to transition into
+            a full-stack role using the MERN stack. I’m excited to contribute to
+            real-world projects and grow as a developer.
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1s2ERKMfizZKN1C6nvNXH5XPmI2ZEckD_/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,232 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxtookitLogo from './assets/tech_logo/redux toolkit.webp'
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import framermotionlogo from './assets/tech_logo/framermotion.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+
+// Experience Section Logo's
+import exp1logo from './assets/experience_logo/exp1.webp';
+
+// Education Section Logo's
+import aceLogo from './assets/education_logo/ace_logo.png';
+import gpaLogo from './assets/education_logo/gpa_logo.webp';
+import sdsLogo from './assets/education_logo/sds_logo.png';
+
+// Project Section Logo's
+import project1Logo from './assets/work_logo/project1.png';
+import project2Logo from './assets/work_logo/project2.png';
+import project3Logo from './assets/work_logo/project3.png';
+import project4Logo from './assets/work_logo/project4.png';
+import project5Logo from './assets/work_logo/project5.png';
+import project6Logo from './assets/work_logo/project6.png';
+import project7Logo from './assets/work_logo/project7.png';
+import project8Logo from './assets/work_logo/project8.png';
+import project9Logo from './assets/work_logo/project9.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux Toolkit', logo: reduxtookitLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+      { name: 'Framer Motion', logo:framermotionlogo}
+    ],
+  },
+  // {
+  //   title: 'Backend',
+  //   skills: [
+  //     { name: 'Springboot', logo: springbootLogo },
+  //     { name: 'Node JS', logo: nodejsLogo },
+  //     { name: 'Express JS', logo: expressjsLogo },
+  //     { name: 'MySQL', logo: mysqlLogo },
+  //     { name: 'MongoDB', logo: mongodbLogo },
+  //     { name: 'Firebase', logo: firebaseLogo },
+  //     { name: 'PostgreSQL', logo: postgreLogo },
+  //   ],
+  // },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Vercel', logo: vercelLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: exp1logo,
+      role: "Web Development (Training)",
+      company: "Solitaire Infosystems Pvt. Ltd.",
+      date: "July 2024 - Sept 2024",
+      desc: "Completed a 45-day Web Development Training Program focused on building core skills in HTML, CSS, JavaScript, and Bootstrap. Gained hands-on experience in creating responsive web pages, designing user-friendly interfaces, and implementing interactive features. This training laid the foundation for frontend development and strengthened understanding of modern web design practices.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Bootstrap",
+      ],
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: aceLogo,
+      school: "Ambala College of Engineering and Applied Research, Ambala",
+      date: "Aug 2022 - June 2025",
+      grade: "8.28 SGPA",
+      desc: "I have completed my Bachelor of Technology (B.Tech) in Computer Science and Engineering from Ambala College of Engineering and Applied Research. During my time at the college, I built a solid foundation in programming, software development, and core computer science concepts. I studied subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I also actively participated in technical workshops and events, which helped enhance my practical knowledge and skills. My experience at Ambala College has played a crucial role in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of Technology - B.Tech ",
+    },
+    {
+      id: 1,
+      img: gpaLogo,
+      school: "Government Polytechnic, Ambala",
+      date: "Aug 2019 - July 2022",
+      grade: "72%",
+      desc: "I completed my Diploma in Automobile Engineering from Government Polytechnic, Ambala City During my studies I gained hands-on experience with vehicle engines and mechanical systems I learned how different automobile parts function and how each contributes to overall performance The program emphasized practical learning including real-world diagnostics and maintenance This experience deepened my interest in automotive technology and mechanical engineering."
+,
+      degree: "Diploma ( Automobile Engineering)",
+    },
+    {
+      id: 2,
+      img: sdsLogo,
+      school: "SD.Public School Nagla Jattan, Ambala",
+      date: "Apr 2018 - March 2019",
+      grade: "71%",
+      desc: "I completed my class 10 education from  SD Public School, Nagla Jattan, under the HBSE board, where I studied Science, Mathematics, Computer Science, and Physical Education.",
+      degree: "HBSE (X) – Science, Mathematics, Computer Science.",
+    },
+    
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Movie-WebApplication",
+      description:
+        "A dynamic and user-friendly Movie Web Application built with React.js, designed to let users explore the latest films and shows. Simply search for any movie title, and the app fetches detailed information including trailers, ratings, release dates, and descriptions using the TMDB API. With a clean UI and smooth user experience, it's the perfect tool for movie lovers to discover and stay updated with trending entertainment.",
+      image: project1Logo,
+      tags: ["React JS", "Tailwind CSS", "API", "Redux Toolkit"],
+      github: "https://github.com/Himanshu6408/Movie-WebApplication-",
+      webapp: "https://movie-web-application-dtlk.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "E-commerce Website for Computer Components",
+      description:
+        "Responsive E-commerce Website for Computer Components Built using HTML and CSS with a clean and organized layout. Fully responsive design for smooth viewing on all devices.",
+      image: project2Logo,
+      tags: ["HTML", "CSS"],
+      github: "https://github.com/Himanshu6408/pc-parts-website",
+      webapp: "https://pc-parts-website.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Refokus-Clone-Website",
+      description:
+        "Refokus Website Clone Developed using React.js, Tailwind CSS, and Framer Motion. Replicates the modern design and fluid animations of Refokus. Demonstrates expertise in frontend development and UI animation.",
+      image: project3Logo,
+      tags: ["React JS",  "Tailwind CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/Himanshu6408/Refokus-Clone-Website/tree/main",
+      webapp: "https://refokus-clone-website.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "GSAP Landing Page Animation",
+      description:
+        "A visually engaging and responsive landing page built using HTML, CSS, and JavaScript. The project utilizes GSAP (GreenSock Animation Platform) to deliver smooth scroll-based and timeline animations. Designed with attention to UI/UX, it showcases interactive transitions and motion effects across devices.",
+      image: project4Logo,
+      tags: ["HTML", "CSS", "JavaScript", "GSAP"],
+      github: "https://github.com/Himanshu6408/GSAP-Project",
+      webapp: "https://gsap-project-sooty.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Random User Profile Generator",
+      description:
+        "A dynamic web application built using HTML, Tailwind CSS, and JavaScript that fetches and displays random user profiles via an external API. It presents user information such as name, email, location, and profile picture, making it a useful project for understanding API integration.",
+      image: project5Logo,
+      tags: ["HTML", "Tailwind CSS", "JavaScript", "API"],
+      github: "https://github.com/Himanshu6408/RandomUser-API/tree/main",
+      webapp: "https://random-user-api-ten.vercel.app/",
+    },
+    {
+      id: 5,
+      title: "3D Spinning UI Animation Project",
+      description:
+        "A visually engaging 3D spinning animation built using HTML and CSS. This project demonstrates creative rotational effects ideal for showcasing products, banners, or interactive UI elements. The smooth animations and clean layout highlight how simple technologies can deliver dynamic visual appeal.",
+      image: project6Logo,
+      tags: ["HTML", "CSS"],
+      github: "https://github.com/Himanshu6408/Rotational-Cane",
+      webapp: "https://rotational-cane.vercel.app/",
+    },
+    {
+      id: 6,
+      title: "MindTap: The Number Game",
+      description:
+        "A fun and interactive number game built with HTML, Tailwind CSS, and JavaScript. Users test their reflexes and number recognition skills by clicking the correct numbers within a time limit. Clean UI and smooth logic make it an engaging project for both learning and entertainment.",
+      image: project7Logo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Himanshu6408/Bubble-Game/tree/main",
+      webapp: "https://bubble-game-one-roan.vercel.app/",
+    },
+    {
+      id: 7,
+      title: "Obysc-Inspired Digital Agency Landing Page",
+      description:
+        "A visually captivating agency website clone inspired by Obysc's design. Built using HTML, CSS, JavaScript, GSAP, and Locomotive Scroll, this project showcases smooth scroll animations, creative layouts, and modern UI interactions that mimic a real-world creative agency portfolio.",
+      image: project8Logo,
+      tags: ["HTML", "CSS", "JavaScript", "GSAP", " Locomotive", "ScrollTrigger"],
+      github: "https://github.com/Himanshu6408/Major-Project-",
+      webapp: "https://major-project-mocha-five.vercel.app/",
+    },
+    {
+      id: 8,
+      title: "Wholesale-Catalog-WebApplication",
+      description:
+        "A React.js-based e-commerce web application built to showcase vehicle components in a structured and professional layout. Developed using Tailwind CSS and JavaScript, the application presents categorized auto parts and accessories for wholesale buyers. It features a clean interface, and smooth user interaction, tailored for desktop use.",
+      image: project9Logo,
+      tags: ["React JS", "Tailwind CSS", "Javascript"],
+      github: "https://github.com/Himanshu6408/Wholesale-Catalog-WebApplication",
+      webapp: "https://wholesale-catalog-web-application.vercel.app/",
+    },
+  ];  
