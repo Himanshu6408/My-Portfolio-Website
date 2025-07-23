3942885e939a78208ae67c66bb9472b14056d2d5
Himanshu Mishra <166356154+Himanshu6408@users.noreply.github.com>
2025-07-23 10:43:20 +0530

    changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -61,7 +61,6 @@ const About = () => {
           </p>
           <a
             href=""
-            target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
@@ -69,7 +68,7 @@ const About = () => {
               boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
             }}
           >
-            DOWNLOAD cV
+            DOWNLOAD CV
           </a>
         </div>
       </div>
