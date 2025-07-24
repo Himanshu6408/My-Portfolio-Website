1f925d5946e364144969c1eaf803f0d75beec86a
Himanshu Mishra <166356154+Himanshu6408@users.noreply.github.com>
2025-07-24 13:38:03 +0530

    changed

--- src/constants.js
+++ src/constants.js
@@ -234,7 +234,7 @@ export const SkillsInfo = [
       id: 9,
       title: "Ecommerce Website",
       description:
-        "A React.js-based eCommerce website designed to professionally showcase men's, women's, and electronics products. Built with Tailwind CSS and JavaScript, the platform features a clean layout optimized for desktop users. Products are well-categorized, enabling smooth navigation and easy browsing for customers. The site focuses on structured product presentation and delivers an intuitive shopping experience.",
+        "A React.js-based eCommerce website designed to professionally showcase men's, women's, and electronics products. Built with Tailwind CSS and JavaScript, the platform features a clean layout optimized for desktop users. Products are well-categorized, enabling smooth navigation and easy browsing for customers.",
       image: project10Logo,
       tags: ["React JS", "Tailwind CSS"],
       github: "https://github.com/Himanshu6408/React-Ecommerce-Website",
