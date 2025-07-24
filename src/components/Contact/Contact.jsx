f50edc7529a8e662c4bdfd5a2c2fb19e8ef7862d
Himanshu Mishra <166356154+Himanshu6408@users.noreply.github.com>
2025-07-24 12:43:32 +0530

    first

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
     emailjs
       .sendForm(
         "service_16fvsv2",  // Replace with your EmailJS Service ID
-        "template_sw0oert",  // Replace with your EmailJS Template ID
+        "template_h2ru3ve",  // Replace with your EmailJS Template ID
         form.current,
         "aEGQ1XWZwK89dT9PL"  // Replace with your EmailJS Public Key
       )
